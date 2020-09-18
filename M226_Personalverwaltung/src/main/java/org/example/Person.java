f084209cae6e9793eb4e7ddfc72ee077f38e6fab
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2020-09-18 08:59:14 +0200

    set get/set methods

--- M226_Personalverwaltung/src/main/java/org/example/Person.java
+++ M226_Personalverwaltung/src/main/java/org/example/Person.java
@@ -11,4 +11,76 @@ public class Person{
     private int m_Eintrittsjahr;
     private double m_Salaer;
     private double m_Pensum;
+
+    public int getM_PersNr() {
+        return m_PersNr;
+    }
+
+    private void setM_PersNr(int m_PersNr) {
+        this.m_PersNr = m_PersNr;
+    }
+
+    public String getM_Andrese() {
+        return m_Andrese;
+    }
+
+    public void setM_Andrese(String m_Andrese) {
+        this.m_Andrese = m_Andrese;
+    }
+
+    public String getM_Name() {
+        return m_Name;
+    }
+
+    public void setM_Name(String m_Name) {
+        this.m_Name = m_Name;
+    }
+
+    public String getM_Vorname() {
+        return m_Vorname;
+    }
+
+    public void setM_Vorname(String m_Vorname) {
+        this.m_Vorname = m_Vorname;
+    }
+
+    public String getM_Plz() {
+        return m_Plz;
+    }
+
+    public void setM_Plz(String m_Plz) {
+        this.m_Plz = m_Plz;
+    }
+
+    public String getM_Ort() {
+        return m_Ort;
+    }
+
+    public void setM_Ort(String m_Ort) {
+        this.m_Ort = m_Ort;
+    }
+
+    public int getM_Eintrittsjahr() {
+        return m_Eintrittsjahr;
+    }
+
+    public void setM_Eintrittsjahr(int m_Eintrittsjahr) {
+        this.m_Eintrittsjahr = m_Eintrittsjahr;
+    }
+
+    public double getM_Salaer() {
+        return m_Salaer;
+    }
+
+    public void setM_Salaer(double m_Salaer) {
+        this.m_Salaer = m_Salaer;
+    }
+
+    public double getM_Pensum() {
+        return m_Pensum;
+    }
+
+    public void setM_Pensum(double m_Pensum) {
+        this.m_Pensum = m_Pensum;
+    }
 }
